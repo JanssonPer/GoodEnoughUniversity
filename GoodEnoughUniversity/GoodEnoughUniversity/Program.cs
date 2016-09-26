22fff95a455cfad12aee313445eb11c591912b65
bratendal <erik.bratendal@gmail.com>
2016-09-26 14:49:59 +0200

    Changed Application.Run to @interface

--- GoodEnoughUniversity/GoodEnoughUniversity/Program.cs
+++ GoodEnoughUniversity/GoodEnoughUniversity/Program.cs
@@ -1,15 +1,24 @@
-﻿using System;
+﻿using GoodEnoughUniversity.View;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Forms;
 using System.Threading.Tasks;
+
 
 namespace GoodEnoughUniversity
 {
     class Program
     {
-        static void Main(string[] args)
+        [STAThread]
+        static void Main()
         {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new @interface());
         }
     }
 }
+
+    
