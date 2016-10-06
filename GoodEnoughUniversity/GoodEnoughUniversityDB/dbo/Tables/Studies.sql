76388ff5a00184eff00746233b0534d4552b40da
bratendal <erik.bratendal@gmail.com>
2016-10-06 20:24:07 +0200

    added trigger to studies

--- GoodEnoughUniversity/GoodEnoughUniversityDB/dbo/Tables/Studies.sql
+++ GoodEnoughUniversity/GoodEnoughUniversityDB/dbo/Tables/Studies.sql
@@ -6,3 +6,23 @@
     FOREIGN KEY ([ssn]) REFERENCES [dbo].[Student] ([ssn])
 );
 
+
+GO
+CREATE TRIGGER CreditLimit 
+ON Studies
+AFTER INSERT
+AS
+DECLARE @ssn varchar(10)
+SELECT @ssn = ssn FROM inserted 
+IF EXISTS(
+			SELECT @ssn, Count(*) AS totalcredits
+				FROM Studies s 
+					INNER JOIN Assignment a
+					ON a.courseCode = s.courseCode
+						GROUP BY ssn
+						HAVING COUNT(*) >= 45)
+BEGIN
+PRINT ('A student can not study more than 45 credits');
+ROLLBACK TRANSACTION;
+RETURN    
+END
