d066a0d5576fc9214d2fee6ad151d90fe083ed99
bratendal <erik.bratendal@gmail.com>
2016-10-11 10:28:28 +0200

    added dal

---
+++ GoodEnoughUniversity/GoodEnoughUniversity/DatabaseAccessLayer/Dal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoodEnoughUniversity.DataBaseAccessLayer
+{
+    class Dal
+    {
+        GoodEnoughUniversityEntities context = new GoodEnoughUniversityEntities();
+
+        public List<Course> GetCourses()
+        {
+            List<Course> list = context.usp_GetCourses().ToList<Course>();
+                return list;
+        }
+
+        public Course GetCourse(String courseCode)
+        {
+            Course c = context.usp_GetCourse(courseCode).SingleOrDefault<Course>();
+                return c;
+        }
+
+    }
+
+   }
+
